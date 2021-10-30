d994f681e8758b2b75c88bbbec2cc59eb9764795
Pavel <koshak087@gmail.com>
2021-10-30 13:01:24 +0300

    add to CsBase 1st HW for lesson 2

---
+++ lesson1/lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите маскимальную температуру за текущие сутки:");
+            double tmax = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите минимальную температуру за текущие сутки:");
+            double tmin = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Средняя температура за сегодня: " + (tmax + tmin) / 2);
+        }
+    }
+}
