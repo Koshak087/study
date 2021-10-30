ea289e9e01486d9cc68592a25bbea0826782b650
Pavel <koshak087@gmail.com>
2021-10-30 15:27:59 +0300

    add 5thHW for lesson 2

---
+++ lesson1/lesson2.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace lesson2._5
+    {
+        class Program
+        {
+            [Flags]
+            public enum Days
+            {
+                Понедельник = 1,
+                Вторник = 2,
+                Среда = 3,
+                Четверг = 4,
+                Пятница = 5,
+                Суббота = 6,
+                Воскресенье = 7
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine($"Офис 1 работает c: {(Days)2} по: {(Days)7}");
+                Console.WriteLine($"Офис 1 работает c: {(Days)1} по: {(Days)7}");
+            Console.ReadLine();
+            }
+        }
+    }
