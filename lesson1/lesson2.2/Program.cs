6257fbb8030e2fa4f841b16c96d58d657f0e0fe2
Pavel <koshak087@gmail.com>
2021-10-30 15:28:17 +0300

    some files changes

--- lesson1/lesson2.2/Program.cs
+++ lesson1/lesson2.2/Program.cs
@@ -6,15 +6,29 @@ namespace lesson2._2
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите маскимальную температуру за текущие сутки:");
+            double tmax = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите минимальную температуру за текущие сутки:");
+            double tmin = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Средняя температура за сегодня: " + (tmax + tmin) / 2);
+            
             Console.Write("Введите номер текущего месяца: ");
             int mon = Convert.ToInt32(Console.ReadLine());
             switch (mon)
             {
                 case 1:
                     Console.WriteLine("Сейчас январь.");
+                    if (tmax+tmin/2 > 0)
+                    {
+                        Console.WriteLine("Доджливая Зима");
+                    }
                     break;
                 case 2:
                     Console.WriteLine("Сейчас февраль.");
+                    if (tmax + tmin / 2 > 0)
+                    {
+                        Console.WriteLine("Доджливая Зима");
+                    }
                     break;
                 case 3:
                     Console.WriteLine("Сейчас март.");
@@ -45,9 +59,16 @@ namespace lesson2._2
                     break;
                 case 12:
                     Console.WriteLine("Сейчас декабрь.");
+                    if (tmax + tmin / 2 > 0)
+                    {
+                        Console.WriteLine("Доджливая Зима");
+                    }
                     break;
             }
             Console.ReadLine();
         }
     }
 }
+
+
+
