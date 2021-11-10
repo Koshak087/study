6bacb4dcf34ee2124a6daec40be8451ab864f679
Pavel <koshak087@gmail.com>
2021-11-10 22:25:24 +0300

    all HW for Lesson 4

---
+++ lesson1/lesson4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson4._1
+{
+    class Program
+    {
+
+        static void Main (string[] args)
+        {
+                GetFullName("Иван", "Иванов", "Ивановчи");
+                GetFullName("Петр", "Петров", "Петрович");
+                GetFullName("Семён", "Семёнов", "Семёнович");
+                GetFullName("Кирилл", "Кириллов", "Кириллович");
+          
+
+        }       
+        private static void GetFullName(string firstName, string lastName, string patronomic)
+        {
+            {
+                Console.WriteLine($"{lastName} {firstName} {patronomic}");
+            }
+            
+        }
+    }
+}
---
+++ lesson1/lesson4.3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace lesson4._3
+{
+    class Program
+
+    {
+        enum Seasons
+        {
+            Зима = 10,
+            Весна = 20,
+            Лето = 30,
+            Осень = 40
+
+        }
+        enum Month
+        {
+            Январь = 1,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+        static void Main(string[] args)
+        {
+            bool control;
+            int ok;
+            string mon;
+            Console.Write("Введите номер текущего месяца: ");
+            do
+            {
+                mon = Console.ReadLine();
+                control = int.TryParse(mon, out ok);
+                if (ok < 1 || ok > 12)
+                {
+                    Console.Write("Ошибка: введите число от 1 до 12: ");
+                    control = false;
+                }
+ 
+            }
+            while (!control);
+            checker(mon);
+        }
+        private static void checker (string c)
+        {
+            int mon;
+            int seas = 0; 
+            mon = Convert.ToInt32(c);
+
+            if (mon == 12 || mon >= 1 || mon <=2)
+            {
+                seas = 10;
+                Console.WriteLine($"Текущий месяц {(Month)mon}, Текущий сезон {(Seasons)seas}");
+            }
+            else if (mon >= 3 || mon <= 5)
+            {
+                seas = 20;
+                Console.WriteLine($"Текущий месяц {(Month)mon}, Текущий сезон {(Seasons)seas}");
+
+            }
+            else if (mon >= 3 || mon <= 5)
+            {
+                seas = 30;
+                Console.WriteLine($"Текущий месяц {(Month)mon}, Текущий сезон {(Seasons)seas}");
+
+            }
+            else if (mon >= 3 || mon <= 5)
+            {
+                seas = 40;
+                Console.WriteLine($"Текущий месяц {(Month)mon}, Текущий сезон {(Seasons)seas}");
+
+            }
+
+
+        }
+
+     }
+}
+
+
