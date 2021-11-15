405e04c0a2a39bef1563efd8444f6fb7322d826b
Pavel <koshak087@gmail.com>
2021-11-15 22:55:30 +0300

    change 4.3 in local, created 5.3

---
+++ lesson1/lesson5.3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+
+namespace lesson5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа от 0 до 255: ");
+            string hw2;
+            string homework2;
+            bool check;
+            int result;
+
+            homework2 = "homework2.bin";
+            do
+            {
+                hw2 = Console.ReadLine();
+                check = int.TryParse(hw2, out result);
+
+
+                if (check)
+                {
+
+                    if (result < 0 || result > 255)
+                    {
+                        Console.Write("Некорректный ввод, ведите число от 0 до 255: ");
+                    }
+
+                }
+
+
+                else
+                
+                    Console.Write("Некорректный ввод, ведите число от 0 до 255: ");
+                
+            }
+            while (!check || result < 0 || result > 255);
+
+            File.WriteAllText(homework2, hw2);
+
+            File.AppendAllText(homework2, Environment.NewLine);
+
+            string hwtext2 = File.ReadAllText(homework2);
+            Console.Write("Вы ввели: ");
+            Console.WriteLine(hwtext2);
+        }
+    }
+}
