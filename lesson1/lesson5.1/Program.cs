762cab196ff2514c23897e0258b8613b6fca9dca
Pavel <koshak087@gmail.com>
2021-11-15 22:14:58 +0300

    created HW 5.2 + change 5.1

--- lesson1/lesson5.1/Program.cs
+++ lesson1/lesson5.1/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.IO.Compression;
 
+
 namespace lesson5._1
 {
     class Program
---
+++ lesson1/lesson5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+namespace lesson5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = "datetime.txt";
+            string [] date = { DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss")};
+            File.AppendAllLines(time, date);
+            string[] datetime = File.ReadAllLines(time);
+            for (int i = 0; i < datetime.Length; i++)
+            {
+                Console.WriteLine(datetime[i]);
+            }
+            
+        }
+    }
+}
