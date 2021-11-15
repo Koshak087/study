460b95336b014cfd55c6426b28eb3777f3bb7776
Pavel <koshak087@gmail.com>
2021-11-15 21:20:39 +0300

    Created HW 5.1

--- lesson1/lesson5.1/Program.cs
+++ lesson1/lesson5.1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 
 namespace lesson5._1
 {
@@ -6,7 +8,19 @@ namespace lesson5._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите текст: ");
+            string hw;
+            string homework;
+           
+            homework = "homework.txt";
+            hw = Console.ReadLine();
+            File.WriteAllText(homework, hw);
+           
+            File.AppendAllText(homework, Environment.NewLine);
+
+            string hwtext = File.ReadAllText(homework);
+            Console.Write("Вы ввели: ");
+            Console.WriteLine(hwtext);
         }
     }
 }
